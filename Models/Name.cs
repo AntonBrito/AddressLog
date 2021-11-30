959665945a13bcc06374d81d21fcd4344316a304
AntonBrito <tonybrito85@gmail.com>
2021-11-29 22:43:43 -0500

    Fixed a typo

--- Models/Name.cs
+++ Models/Name.cs
@@ -1,14 +1,14 @@
 using System;
 using System.Collections.Generic;
 
-namespace ContactLog.Models
+namespace AddressLog.Models
 {
     public class Name
     {
         public int ID { get; set; }
         public string LastName { get; set; }
         public string FirstName { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
+
+        public ICollection<Contact> Contacts { get; set; }
     }
 }
